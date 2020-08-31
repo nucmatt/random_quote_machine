e8ef2f56d5bfe209549991d1130fd37abc1f4dcd
Matthew Wessel <nucmatt@gmail.com>
2020-08-31 11:29:38 -0500

    Installed node sass as a dependency for the project and renamed App.css to App.scss.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import './App.css';
+import './App.scss';
 
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-			
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
