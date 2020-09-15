d21477ad5eae5c938d11e2eb988e4bf1a57a3465
Matthew Wessel <nucmatt@gmail.com>
2020-09-15 12:13:49 -0500

    Added basic html structure to App.js.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
+		<div className="container bg-primary">
+			<h1 className="text-center">Random Quote Machine</h1>
+			<div id="quote-box" className="card text-center">
+				<p id="text">Quote goes here</p>
+				<p id="author">Author goes here</p>
+				<div className="">
+					<button id="tweet-quote" className="btn">Tweet quote</button>
+					<button id="new-quote" className="btn">New Quote</button>
+				</div>
+			</div>
+			<footer>
+				<p>Created by <a href=""></a>Matthew Wessel</p>
+				<p id="socials">Socials Go here</p>
+			</footer>
 		</div>
 	);
 }
