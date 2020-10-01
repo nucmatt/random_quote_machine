5df248f97056405da8531d7f4554c00dbd8cac56
Matthew Wessel <nucmatt@gmail.com>
2020-10-01 11:44:13 -0500

    Changed quote source to use FreeCodeCamp camperbot. Adjusted fetchQuote to utilize new format.

--- src/App.js
+++ src/App.js
@@ -10,14 +10,16 @@ function App() {
 		fetchQuote();
 	}, []);
 
-	const quoteURL = 'https://quotes.stormconsultancy.co.uk/random.json';
+	const quoteURL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
 	const fetchQuote = () => {
 		fetch(quoteURL)
 			.then((response) => response.json())
 			.then((json) => {
-				setQuote(json.quote);
-				setAuthor('- ' + json.author);
+				let randNum = () => Math.floor(Math.random() * 10);
+				let quote = json.quotes[randNum()];
+				setQuote(quote.quote);
+				setAuthor('- ' + quote.author);
 			});
 	};
 
